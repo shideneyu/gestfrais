5172df48f1b2d5ea0b445f3eacf9ec3186e38598
sidney <shideneyu@sidney.tortank>
2012-10-04 15:41:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gestfrais::Application.config.secret_token = '7806d4afbd969b78935e7e238d325da539ce7e95caddf56ac22c6a9e287c99196bd33edb23b5645429f810d83716f3318309e855695dd7d8d6de5a93d4bf3c6c'
